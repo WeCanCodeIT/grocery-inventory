f06a5d4d6d54e7948689cce8fa36e8fc6f10bce8
AaronSmithX <instructor@wecancodeit.org>
2018-06-14 10:00:42 -0400

    Update guide

---
+++ src/test/java/org/wecancodeit/groceryinventory/GroceryInventoryControllerTest.java
@@ -0,0 +1,9 @@
+package org.wecancodeit.groceryinventory;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class GroceryInventoryControllerTest {
+
+}
