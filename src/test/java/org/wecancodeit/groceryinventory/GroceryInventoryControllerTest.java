0ba4d4d1f6b767fd5de60f8619f402fa2042c24b
Don Hamilton III <donhamiltoniii@gmail.com>
2018-06-18 08:55:17 -0400

    Fix testing error and add CSS

--- src/test/java/org/wecancodeit/groceryinventory/GroceryInventoryControllerTest.java
+++ src/test/java/org/wecancodeit/groceryinventory/GroceryInventoryControllerTest.java
@@ -1,6 +1,12 @@
 package org.wecancodeit.groceryinventory;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import javax.annotation.Resource;
 
@@ -11,10 +17,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.hamcrest.Matchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(GroceryInventoryController.class)
@@ -45,17 +47,20 @@ public class GroceryInventoryControllerTest {
 	}
 	
 	@Test
-	public void getProductShouldReturnProductsView() throws Exception {
+	public void getProductShouldReturnProductView() throws Exception {
+		given(repo.findById(42L)).willReturn(product);
 		mvc.perform(get("/products/42")).andExpect(view().name(is(equalTo("product"))));
 	}
 	
 	@Test
 	public void getProductShouldBeOk() throws Exception {
+		given(repo.findById(42L)).willReturn(product);
 		mvc.perform(get("/products/42")).andExpect(status().is2xxSuccessful());
 	}
 	
 	@Test
 	public void getProductShouldReturnOneProduct() throws Exception {
+		given(repo.findById(42L)).willReturn(product);
 		mvc.perform(get("/products/42")).andExpect(model().attribute("product", is(repo.findById(42L))));
 	}
 }
